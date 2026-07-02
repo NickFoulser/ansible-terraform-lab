eb59e6aded4607d66ad8a138b1fadd48f12504e8
Nick Foulser <nickfoulser@hotmail.com>
2026-07-02 21:13:59 +0100

    Adding the updated inventory file

--- terraform/inventory.tf
+++ terraform/inventory.tf
@@ -1,21 +1,22 @@
 resource "local_file" "ansible_inventory" {
-  content = <<EOF
+
+content = <<EOF
 [ubuntu]
-${aws_instance.ubuntu_web.public_ip} ansible_user=ubuntu server_environment=${aws_instance.ubuntu_web.tags["Environment"]} server_hostname=${aws_instance.ubuntu_web.tags["Name"]}
+${aws_instance.ubuntu_web.tags["Name"]} ansible_host=${aws_instance.ubuntu_web.public_ip} ansible_user=ubuntu server_environment=${aws_instance.ubuntu_web.tags["Environment"]}
 
 [amazon]
-${aws_instance.amazon_web.public_ip} ansible_user=ec2-user server_environment=${aws_instance.amazon_web.tags["Environment"]} server_hostname=${aws_instance.amazon_web.tags["Name"]}
+${aws_instance.amazon_web.tags["Name"]} ansible_host=${aws_instance.amazon_web.public_ip} ansible_user=ec2-user server_environment=${aws_instance.amazon_web.tags["Environment"]}
 
 [jenkins]
-${aws_instance.jenkins.public_ip} ansible_user=ec2-user server_environment=${aws_instance.jenkins.tags["Environment"]} server_hostname=${aws_instance.jenkins.tags["Name"]}
+${aws_instance.jenkins.tags["Name"]} ansible_host=${aws_instance.jenkins.public_ip} ansible_user=ec2-user server_environment=${aws_instance.jenkins.tags["Environment"]}
 
 [web:children]
 ubuntu
 amazon
 
 [all:vars]
-ansible_ssh_private_key_file=~/.ssh/database_test.pem
 ansible_python_interpreter=/usr/bin/python3
+ansible_ssh_private_key_file=~/.ssh/database_test.pem
 EOF
 
   filename = "../ansible/inventory.ini"
--- terraform/main.tf
+++ terraform/main.tf
@@ -84,6 +84,12 @@ resource "aws_instance" "jenkins" {
   key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.web.id]
 
+    root_block_device {
+
+    volume_size = 8
+
+  }
+
   tags = {
     Name        = "jenkins01"
     Environment = "lab"
